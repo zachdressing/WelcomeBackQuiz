f9aa521bb693831a9e49668575fe3994275b00b0
zachdressing <zed_dressel@hotmail.com>
2023-11-06 09:31:56 -0800

    centered

--- assets/script/script.js
+++
@@ -1,6 +0,0 @@
-$('#spinnerButton').click(function() {
-    $(this).each(function(){
-        $('#animationelement').removeClass(); 
-        $('#animationelement').addClass("spinner");
-      });
-  });
